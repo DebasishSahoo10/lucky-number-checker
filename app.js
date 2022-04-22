d1f24d438868bb25fa0cbe01e4998991af62d01a
Debasish Sahoo <100151159+DebasishSahoo10@users.noreply.github.com>
2022-04-22 20:04:05 +0530

    feat : added calculation

--- app.js
+++ app.js
@@ -1,4 +1,33 @@
 var date = document.querySelector ('#birth-date')
 var number = document.querySelector ('#lucky-number')
 var button = document.querySelector ('#check-button') 
-var output = document.queryCommandValue ('#output-box')
+var output = document.querySelector ('#output-box')
+
+
+
+
+function checker() {
+    var numbersFromDate = date.value.replaceAll("-","");
+    let sum = 0;
+    for (let i=0; i<date.value.length; i++) {
+        sum = sum + Number(numbersFromDate.charAt(i));
+    }
+    if (sum%number.value === 0) {
+        output.innerText = "Yayyy. Your Birthdate is lucky";
+        console.log("lucky")
+    } else {
+        output.innerText = "Sorry. Your Birthdate is not that lucky";
+    }
+
+    console.log (sum);
+}
+
+
+
+
+
+
+
+
+
+button.addEventListener ("click", checker)
