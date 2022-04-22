03e9e9538b8b21469fa880b90a268c9feb023611
Debasish Sahoo <100151159+DebasishSahoo10@users.noreply.github.com>
2022-04-22 18:35:28 +0530

    refactor : linked html elements

---
+++ app.js
@@ -0,0 +1,4 @@
+var date = document.querySelector ('#birth-date')
+var number = document.querySelector ('#lucky-number')
+var button = document.querySelector ('#check-button') 
+var output = document.queryCommandValue ('#output-box')
